743b83282accb8d067bd702374aacd96797aeca6
RafaelNarvaez <r-enc@hotmail.com>
2022-04-19 14:32:24 -0400

    1st commit

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,7 @@
+# Example of how to create a File
+file { '/tmp/school':
+    ensure => 'present',
+    mode => '0744',
+    owner => 'www-data',
+    group => 'www-data',
+    content => 'I love Puppet', }
