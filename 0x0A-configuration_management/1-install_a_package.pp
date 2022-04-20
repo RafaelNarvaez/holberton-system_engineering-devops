e4fdc6ffb35c45ccd03a45445972a63c47ee45ac
RafaelNarvaez <r-enc@hotmail.com>
2022-04-20 12:01:19 -0400

    Fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 # The Puppet Installer
 
-package { 'puppet-lint':
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'gem',
-  source   => 'https://rubygems.org',
-}
+  provider => 'pip3',
+  }
