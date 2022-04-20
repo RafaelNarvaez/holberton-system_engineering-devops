1a140c609cfafc009d2698448ce581a867197477
RafaelNarvaez <r-enc@hotmail.com>
2022-04-20 14:16:20 -0400

    Fixed code

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,13 @@
 # How to config an ssh file
-$str = "host *
-  ForwardAgent yes
-  ForwardX11 yes
-  IdentityFile ~/.ssh/school
-"
-file {'etc/ssh/ssh_config':
+
+include stdlib
+file_line {'change private key':
+  ensure => 'present',
+  path   => 'etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+}
+file_line {'no password':
   ensure  => 'present',
   path    => '/etc/ssh/ssh_config',
-  content => $str,
-  mode    => '0755',
+  line    => 'PasswordAuthentication no',
 }
